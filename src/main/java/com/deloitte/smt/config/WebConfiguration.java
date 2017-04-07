7321d36610fa6e477e5ed9ef6fec06e50c43cd9b
Mahesh Chandra <myelleswarapu@innominds.com>
2017-04-07 12:18:30 +0530

    Added CORS support.

---
+++ src/main/java/com/deloitte/smt/config/WebConfiguration.java
@@ -0,0 +1,24 @@
+package com.deloitte.smt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by myelleswarapu on 07-04-2017.
+ */
+@Configuration
+public class WebConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/camunda/api/**").allowedOrigins("*");
+            }
+        };
+    }
+}
