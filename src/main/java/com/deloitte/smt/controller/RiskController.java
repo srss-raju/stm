ee018c9e2bb42cacf695b2649449895b8b2bea2f
RajeshKumar B <rbongurala@innominds.com>
2017-04-13 10:51:43 +0530

    Get ALL Risk Plans by status

--- src/main/java/com/deloitte/smt/controller/RiskController.java
+++ src/main/java/com/deloitte/smt/controller/RiskController.java
@@ -1,11 +1,17 @@
 package com.deloitte.smt.controller;
 
+import java.util.List;
+
+import com.deloitte.smt.entity.AssessmentPlan;
 import com.deloitte.smt.entity.RiskPlan;
 import com.deloitte.smt.service.RiskPlanService;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -22,5 +28,10 @@ public class RiskController {
     public String createRiskPlan(@RequestBody RiskPlan riskPlan) {
         riskPlanService.insert(riskPlan);
         return "Successfully Created";
+    }
+    
+    @GetMapping(value = "/allRiskPlans")
+    public List<RiskPlan> getAllRiskPlans(@RequestParam(value = "status", required = false) String status){
+        return riskPlanService.findAllRiskPlansByStatus(status);
     }
 }
