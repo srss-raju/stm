6128ec57a796fd045932d8038a0f8ea33adacfb2
RajeshKumar B <rbongurala@innominds.com>
2017-05-11 15:56:45 +0530

    Modified

--- src/test/java/com/deloitte/smt/SignalManagmentProcessTests.java
+++ src/test/java/com/deloitte/smt/SignalManagmentProcessTests.java
@@ -1,15 +1,12 @@
 package com.deloitte.smt;
 
-import com.deloitte.smt.dto.SearchDto;
-import com.deloitte.smt.entity.Topic;
-import com.deloitte.smt.service.SignalService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+import com.deloitte.smt.service.SignalService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -22,20 +19,5 @@ public class SignalManagmentProcessTests {
 	public void contextLoads() {
 	}
 	
-	@Test
-	public void testCounts(){
-		Long validateCount = signalService.getValidateAndPrioritizeCount();
-		Long assesmentCount = signalService.getAssessmentCount();
-		Long riskCount = signalService.getRiskCount();
-		System.out.println("validateCount  -->> "+validateCount);
-		System.out.println("assesmentCount  -->> "+assesmentCount);
-		System.out.println("riskCount  -->> "+riskCount);
-	}
-	
-	@Test
-	public void testGetAllByStatus(){
-		List<Topic> list = signalService.findAllForSearch(new SearchDto());
-		System.out.println("validateCount  -->> "+list);
-	}
 
 }
