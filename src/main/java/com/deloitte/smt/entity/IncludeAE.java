6e592a00611975367b14897059c691adbfc97bdf
RajeshKumar B <rbongurala@innominds.com>
2017-05-12 18:12:24 +0530

    IncludeAE CRUD operations

--- src/main/java/com/deloitte/smt/entity/IncludeAE.java
+++ src/main/java/com/deloitte/smt/entity/IncludeAE.java
@@ -1,5 +1,7 @@
 package com.deloitte.smt.entity;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -15,6 +17,9 @@ public class IncludeAE {
     private Long id;
 	private String aeName;
 	private Long detectionId;
+	private Date createdDate;
+    private String createdBy;
+    private Date lastModifiedDate;
 	
 	public Long getId() {
 		return id;
@@ -33,5 +38,23 @@ public class IncludeAE {
 	}
 	public void setAeName(String aeName) {
 		this.aeName = aeName;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public Date getLastModifiedDate() {
+		return lastModifiedDate;
+	}
+	public void setLastModifiedDate(Date lastModifiedDate) {
+		this.lastModifiedDate = lastModifiedDate;
 	}
 }
