630bfcfd4e26edd2f9d54555afaf87a52c545da8
RajeshKumar B <rbongurala@innominds.com>
2017-05-25 17:46:48 +0530

    Source Name condition changed

--- src/main/java/com/deloitte/smt/service/SignalMatchService.java
+++ src/main/java/com/deloitte/smt/service/SignalMatchService.java
@@ -79,17 +79,22 @@ public class SignalMatchService {
                 }
             }
         }
-        StringBuilder queryBuilder = new StringBuilder("select signal.* from sm_topic signal INNER JOIN sm_ingredient ing ON  (signal.id = ing.topic_id) LEFT OUTER JOIN  sm_pt pt ON (signal.id = pt.topic_id )  where signal.created_date < ?  and ing.ingredient_name=? and source_name =?");
+        StringBuilder queryBuilder = new StringBuilder("select signal.* from sm_topic signal INNER JOIN sm_ingredient ing ON  (signal.id = ing.topic_id) LEFT OUTER JOIN  sm_pt pt ON (signal.id = pt.topic_id )  where signal.created_date < ?  and ing.ingredient_name=? ");
+        if(topic.getSourceName() != null){
+        	queryBuilder.append(" and source_name =\'");
+        	queryBuilder.append(topic.getSourceName());
+        	queryBuilder.append("\'");
+        }
         if(!StringUtils.isEmpty(builder.toString())){
         	queryBuilder.append(" and pt.pt_name IN (");
         	queryBuilder.append(builder.toString());
         	queryBuilder.append(")");
         } 
-        queryBuilder.append("order by signal.created_date desc");
+        queryBuilder.append(" order by signal.created_date desc");
 		Query q = entityManager.createNativeQuery(queryBuilder.toString(),Topic.class);
 		q.setParameter(1, topic.getCreatedDate());
 		q.setParameter(2, topic.getIngredient().getIngredientName());
-		q.setParameter(3, topic.getSourceName());
+		//q.setParameter(3, topic.getSourceName());
 		List<Topic> signals = (List<Topic>)q.getResultList();
 		return signals;
 	}
