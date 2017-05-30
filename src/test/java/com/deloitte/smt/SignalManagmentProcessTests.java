9c6e8a5495dda0d2f075505e6c5e314ec4ae13ba
RajeshKumar B <rbongurala@innominds.com>
2017-05-30 23:57:11 +0530

    Testcase commented

--- src/test/java/com/deloitte/smt/SignalManagmentProcessTests.java
+++ src/test/java/com/deloitte/smt/SignalManagmentProcessTests.java
@@ -2,18 +2,14 @@ package com.deloitte.smt;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-
-import com.deloitte.smt.service.SignalService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SignalManagmentProcessTests {
 	
-	@Autowired
-	SignalService signalService;
+	
 
 	@Test
 	public void contextLoads() {
