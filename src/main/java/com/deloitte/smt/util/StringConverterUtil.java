b949e1ddf868ebe02f43b6bc1f39d6c4010733fb
RajeshKumar B <rbongurala@innominds.com>
2017-06-15 00:47:06 +0530

    private constructor added

--- src/main/java/com/deloitte/smt/util/StringConverterUtil.java
+++ src/main/java/com/deloitte/smt/util/StringConverterUtil.java
@@ -8,6 +8,7 @@ import java.util.regex.Pattern;
  * Created by myelleswarapu on 01-05-2017.
  */
 public class StringConverterUtil {
+	private StringConverterUtil(){}
 
     public static List<String> convertStringToList(String orgVal) {
         return Arrays.asList(orgVal.split(Pattern.quote(Constants.MULTI_VALUE_SEPARATOR)));
