e954c79869545a5d5cc5a0554aa11e1814ea41c0
RajeshKumar B <rbongurala@innominds.com>
2017-06-15 00:50:18 +0530

    unused import removed

--- src/main/java/com/deloitte/smt/service/TaskTemplateService.java
+++ src/main/java/com/deloitte/smt/service/TaskTemplateService.java
@@ -8,7 +8,6 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.deloitte.smt.entity.SignalAction;
 import com.deloitte.smt.entity.TaskTemplate;
