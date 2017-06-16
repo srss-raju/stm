7651674535a033a3b3efac8fad526148aac73cba
RajeshKumar B <rbongurala@innominds.com>
2017-06-16 15:22:25 +0530

    private constructor is added

--- src/main/java/com/deloitte/smt/util/Constants.java
+++ src/main/java/com/deloitte/smt/util/Constants.java
@@ -4,6 +4,8 @@ package com.deloitte.smt.util;
  * Created by myelleswarapu on 01-05-2017.
  */
 public class Constants {
+	
+	private Constants(){}
 
     public static final String MULTI_VALUE_SEPARATOR = "|";
 }
