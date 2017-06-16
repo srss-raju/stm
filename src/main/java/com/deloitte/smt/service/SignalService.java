45eabe7b62e2a6c5a6ac9b2e9e9ef20bd3834d7a
RajeshKumar B <rbongurala@innominds.com>
2017-06-16 15:32:46 +0530

    Removed Cyclomatic

--- src/main/java/com/deloitte/smt/service/SignalService.java
+++ src/main/java/com/deloitte/smt/service/SignalService.java
@@ -413,89 +413,18 @@ public class SignalService {
 		if (null != searchDto) {
 			List<Predicate> predicates = new ArrayList<>(10);
 			
-			if (!CollectionUtils.isEmpty(searchDto.getProducts())) {
-				Root<Product> rootProduct = query.from(Product.class);
-				Predicate productEquals = criteriaBuilder.equal(rootTopic.get("id"), rootProduct.get(SmtConstant.TOPIC_ID.getDescription()));
-				Predicate producNameEquals = criteriaBuilder.isTrue(rootProduct.get("productName").in(searchDto.getProducts()));
-				predicates.add(productEquals);
-				predicates.add(producNameEquals);
-			}
-			
-			if (!CollectionUtils.isEmpty(searchDto.getLicenses())) {
-				Root<License> rootLicense = query.from(License.class);
-				Predicate licenseEquals = criteriaBuilder.equal(rootTopic.get("id"), rootLicense.get(SmtConstant.TOPIC_ID.getDescription()));
-				Predicate licenseNameEquals = criteriaBuilder.isTrue(rootLicense.get("licenseName").in(searchDto.getLicenses()));
-				predicates.add(licenseEquals);
-				predicates.add(licenseNameEquals);
-			}
-			
-			if (!CollectionUtils.isEmpty(searchDto.getIngredients())) {
-				Root<Ingredient> rootIngredient = query.from(Ingredient.class);
-				Predicate ingredientEquals = criteriaBuilder.equal(rootTopic.get("id"), rootIngredient.get(SmtConstant.TOPIC_ID.getDescription()));
-				Predicate ingredientNameEquals = criteriaBuilder.isTrue(rootIngredient.get("ingredientName").in(searchDto.getIngredients()));
-				predicates.add(ingredientEquals);
-				predicates.add(ingredientNameEquals);
-			}
-			
-			
-			if (!CollectionUtils.isEmpty(searchDto.getHlts())) {
-				Root<Hlt> rootHlt = query.from(Hlt.class);
-				Predicate hltTopicEquals = criteriaBuilder.equal(rootTopic.get("id"), rootHlt.get(SmtConstant.TOPIC_ID.getDescription()));
-				Predicate hltNameEquals = criteriaBuilder.isTrue(rootHlt.get("hltName").in(searchDto.getHlts()));
-				predicates.add(hltTopicEquals);
-				predicates.add(hltNameEquals);
-			}
-
-			if (!CollectionUtils.isEmpty(searchDto.getHlgts())) {
-				Root<Hlgt> rootHlgt = query.from(Hlgt.class);
-				Predicate hlgtTopicEquals = criteriaBuilder.equal(rootTopic.get("id"), rootHlgt.get(SmtConstant.TOPIC_ID.getDescription()));
-				Predicate hlgtNameEquals = criteriaBuilder.isTrue(rootHlgt.get("hlgtName").in(searchDto.getHlgts()));
-				predicates.add(hlgtTopicEquals);
-				predicates.add(hlgtNameEquals);
-			}
-
-			if (!CollectionUtils.isEmpty(searchDto.getPts())) {
-				Root<Pt> rootPt = query.from(Pt.class);
-				Predicate ptTopicEquals = criteriaBuilder.equal(rootTopic.get("id"), rootPt.get(SmtConstant.TOPIC_ID.getDescription()));
-				Predicate ptNameEquals = criteriaBuilder.isTrue(rootPt.get("ptName").in(searchDto.getPts()));
-				predicates.add(ptTopicEquals);
-				predicates.add(ptNameEquals);
-			}
-
-			if (null != searchDto.getCreatedDate()) {
-				predicates.add(criteriaBuilder.equal(rootTopic.get(SmtConstant.CREATED_DATE.getDescription()), searchDto.getCreatedDate()));
-			}
-
-			if (!CollectionUtils.isEmpty(searchDto.getStatuses())) {
-				predicates.add(criteriaBuilder.isTrue(rootTopic.get("signalStatus").in(searchDto.getStatuses())));
-			}
-
-			if (!CollectionUtils.isEmpty(searchDto.getSignalNames())) {
-				predicates.add(criteriaBuilder.isTrue(rootTopic.get("name").in(searchDto.getSignalNames())));
-			}
-
-			if (!CollectionUtils.isEmpty(searchDto.getAssignees())) {
-				predicates.add(criteriaBuilder.isTrue(rootTopic.get("assignTo").in(searchDto.getAssignees())));
-			}
-
-			if (null != searchDto.getStartDate() && null != searchDto.getEndDate()) {
-				if (searchDto.isDueDate()) {
-					predicates.add(
-							criteriaBuilder.greaterThanOrEqualTo(rootTopic.get("dueDate"), searchDto.getStartDate()));
-					predicates.add(criteriaBuilder.lessThanOrEqualTo(rootTopic.get("dueDate"), searchDto.getEndDate()));
-				} else {
-					predicates.add(criteriaBuilder.greaterThanOrEqualTo(rootTopic.get(SmtConstant.CREATED_DATE.getDescription()),
-							searchDto.getStartDate()));
-					predicates.add(
-							criteriaBuilder.lessThanOrEqualTo(rootTopic.get(SmtConstant.CREATED_DATE.getDescription()), searchDto.getEndDate()));
-				}
-
-			}
-
-			if (!CollectionUtils.isEmpty(searchDto.getSignalConfirmations())) {
-				predicates.add(criteriaBuilder
-						.isTrue(rootTopic.get("signalConfirmation").in(searchDto.getSignalConfirmations())));
-			}
+			addProducts(searchDto, criteriaBuilder, query, rootTopic, predicates);
+			addLicenses(searchDto, criteriaBuilder, query, rootTopic, predicates);
+			addIngredients(searchDto, criteriaBuilder, query, rootTopic, predicates);
+			addHlts(searchDto, criteriaBuilder, query, rootTopic, predicates);
+			addHlgts(searchDto, criteriaBuilder, query, rootTopic, predicates);
+			addPts(searchDto, criteriaBuilder, query, rootTopic, predicates);
+			addCreatedDate(searchDto, criteriaBuilder, rootTopic, predicates);
+			addStatuses(searchDto, criteriaBuilder, rootTopic, predicates);
+			addSignalNames(searchDto, criteriaBuilder, rootTopic, predicates);
+			addAssignees(searchDto, criteriaBuilder, rootTopic, predicates);
+			addDueDate(searchDto, criteriaBuilder, rootTopic, predicates);
+			addSignalConfirmations(searchDto, criteriaBuilder, rootTopic, predicates);
 
 			Predicate andPredicate = criteriaBuilder.and(predicates.toArray(new Predicate[predicates.size()]));
 			query.select(rootTopic).where(andPredicate).orderBy(criteriaBuilder.desc(rootTopic.get(SmtConstant.CREATED_DATE.getDescription())))
@@ -506,6 +435,215 @@ public class SignalService {
 		
 		TypedQuery<Topic> q = entityManager.createQuery(query);
 		return q.getResultList();
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addSignalConfirmations(SearchDto searchDto,
+			CriteriaBuilder criteriaBuilder, Root<Topic> rootTopic,
+			List<Predicate> predicates) {
+		if (!CollectionUtils.isEmpty(searchDto.getSignalConfirmations())) {
+			predicates.add(criteriaBuilder
+					.isTrue(rootTopic.get("signalConfirmation").in(searchDto.getSignalConfirmations())));
+		}
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addDueDate(SearchDto searchDto,
+			CriteriaBuilder criteriaBuilder, Root<Topic> rootTopic,
+			List<Predicate> predicates) {
+		if (null != searchDto.getStartDate() && null != searchDto.getEndDate()) {
+			if (searchDto.isDueDate()) {
+				predicates.add(
+						criteriaBuilder.greaterThanOrEqualTo(rootTopic.get("dueDate"), searchDto.getStartDate()));
+				predicates.add(criteriaBuilder.lessThanOrEqualTo(rootTopic.get("dueDate"), searchDto.getEndDate()));
+			} else {
+				predicates.add(criteriaBuilder.greaterThanOrEqualTo(rootTopic.get(SmtConstant.CREATED_DATE.getDescription()),
+						searchDto.getStartDate()));
+				predicates.add(
+						criteriaBuilder.lessThanOrEqualTo(rootTopic.get(SmtConstant.CREATED_DATE.getDescription()), searchDto.getEndDate()));
+			}
+
+		}
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addAssignees(SearchDto searchDto,
+			CriteriaBuilder criteriaBuilder, Root<Topic> rootTopic,
+			List<Predicate> predicates) {
+		if (!CollectionUtils.isEmpty(searchDto.getAssignees())) {
+			predicates.add(criteriaBuilder.isTrue(rootTopic.get("assignTo").in(searchDto.getAssignees())));
+		}
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addSignalNames(SearchDto searchDto,
+			CriteriaBuilder criteriaBuilder, Root<Topic> rootTopic,
+			List<Predicate> predicates) {
+		if (!CollectionUtils.isEmpty(searchDto.getSignalNames())) {
+			predicates.add(criteriaBuilder.isTrue(rootTopic.get("name").in(searchDto.getSignalNames())));
+		}
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addStatuses(SearchDto searchDto,
+			CriteriaBuilder criteriaBuilder, Root<Topic> rootTopic,
+			List<Predicate> predicates) {
+		if (!CollectionUtils.isEmpty(searchDto.getStatuses())) {
+			predicates.add(criteriaBuilder.isTrue(rootTopic.get("signalStatus").in(searchDto.getStatuses())));
+		}
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addCreatedDate(SearchDto searchDto,
+			CriteriaBuilder criteriaBuilder, Root<Topic> rootTopic,
+			List<Predicate> predicates) {
+		if (null != searchDto.getCreatedDate()) {
+			predicates.add(criteriaBuilder.equal(rootTopic.get(SmtConstant.CREATED_DATE.getDescription()), searchDto.getCreatedDate()));
+		}
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param query
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addPts(SearchDto searchDto, CriteriaBuilder criteriaBuilder,
+			CriteriaQuery<Topic> query, Root<Topic> rootTopic,
+			List<Predicate> predicates) {
+		if (!CollectionUtils.isEmpty(searchDto.getPts())) {
+			Root<Pt> rootPt = query.from(Pt.class);
+			Predicate ptTopicEquals = criteriaBuilder.equal(rootTopic.get("id"), rootPt.get(SmtConstant.TOPIC_ID.getDescription()));
+			Predicate ptNameEquals = criteriaBuilder.isTrue(rootPt.get("ptName").in(searchDto.getPts()));
+			predicates.add(ptTopicEquals);
+			predicates.add(ptNameEquals);
+		}
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param query
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addHlgts(SearchDto searchDto, CriteriaBuilder criteriaBuilder,
+			CriteriaQuery<Topic> query, Root<Topic> rootTopic,
+			List<Predicate> predicates) {
+		if (!CollectionUtils.isEmpty(searchDto.getHlgts())) {
+			Root<Hlgt> rootHlgt = query.from(Hlgt.class);
+			Predicate hlgtTopicEquals = criteriaBuilder.equal(rootTopic.get("id"), rootHlgt.get(SmtConstant.TOPIC_ID.getDescription()));
+			Predicate hlgtNameEquals = criteriaBuilder.isTrue(rootHlgt.get("hlgtName").in(searchDto.getHlgts()));
+			predicates.add(hlgtTopicEquals);
+			predicates.add(hlgtNameEquals);
+		}
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param query
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addHlts(SearchDto searchDto, CriteriaBuilder criteriaBuilder,
+			CriteriaQuery<Topic> query, Root<Topic> rootTopic,
+			List<Predicate> predicates) {
+		if (!CollectionUtils.isEmpty(searchDto.getHlts())) {
+			Root<Hlt> rootHlt = query.from(Hlt.class);
+			Predicate hltTopicEquals = criteriaBuilder.equal(rootTopic.get("id"), rootHlt.get(SmtConstant.TOPIC_ID.getDescription()));
+			Predicate hltNameEquals = criteriaBuilder.isTrue(rootHlt.get("hltName").in(searchDto.getHlts()));
+			predicates.add(hltTopicEquals);
+			predicates.add(hltNameEquals);
+		}
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param query
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addIngredients(SearchDto searchDto,
+			CriteriaBuilder criteriaBuilder, CriteriaQuery<Topic> query,
+			Root<Topic> rootTopic, List<Predicate> predicates) {
+		if (!CollectionUtils.isEmpty(searchDto.getIngredients())) {
+			Root<Ingredient> rootIngredient = query.from(Ingredient.class);
+			Predicate ingredientEquals = criteriaBuilder.equal(rootTopic.get("id"), rootIngredient.get(SmtConstant.TOPIC_ID.getDescription()));
+			Predicate ingredientNameEquals = criteriaBuilder.isTrue(rootIngredient.get("ingredientName").in(searchDto.getIngredients()));
+			predicates.add(ingredientEquals);
+			predicates.add(ingredientNameEquals);
+		}
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param query
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addLicenses(SearchDto searchDto,
+			CriteriaBuilder criteriaBuilder, CriteriaQuery<Topic> query,
+			Root<Topic> rootTopic, List<Predicate> predicates) {
+		if (!CollectionUtils.isEmpty(searchDto.getLicenses())) {
+			Root<License> rootLicense = query.from(License.class);
+			Predicate licenseEquals = criteriaBuilder.equal(rootTopic.get("id"), rootLicense.get(SmtConstant.TOPIC_ID.getDescription()));
+			Predicate licenseNameEquals = criteriaBuilder.isTrue(rootLicense.get("licenseName").in(searchDto.getLicenses()));
+			predicates.add(licenseEquals);
+			predicates.add(licenseNameEquals);
+		}
+	}
+
+	/**
+	 * @param searchDto
+	 * @param criteriaBuilder
+	 * @param query
+	 * @param rootTopic
+	 * @param predicates
+	 */
+	private void addProducts(SearchDto searchDto,
+			CriteriaBuilder criteriaBuilder, CriteriaQuery<Topic> query,
+			Root<Topic> rootTopic, List<Predicate> predicates) {
+		if (!CollectionUtils.isEmpty(searchDto.getProducts())) {
+			Root<Product> rootProduct = query.from(Product.class);
+			Predicate productEquals = criteriaBuilder.equal(rootTopic.get("id"), rootProduct.get(SmtConstant.TOPIC_ID.getDescription()));
+			Predicate producNameEquals = criteriaBuilder.isTrue(rootProduct.get("productName").in(searchDto.getProducts()));
+			predicates.add(productEquals);
+			predicates.add(producNameEquals);
+		}
 	}
 
     public Long getValidateAndPrioritizeCount(String assignTo){
@@ -542,11 +680,7 @@ public class SignalService {
 			for (Long id : assessmentPlan.getTemplateIds()) {
 				List<SignalAction> actions = assessmentActionRepository.findAllByTemplateId(id);
 				signalActionList = new ArrayList<>();
-				if (!CollectionUtils.isEmpty(actions)) {
-					for (SignalAction action : actions) {
-						createAssessmentAction(assessmentPlan, sort,signalActionList, action);
-					}
-				}
+				createAssessmentActions(assessmentPlan, sort, signalActionList, actions);
 			}
 		}
 		return signalActionList;
@@ -556,10 +690,23 @@ public class SignalService {
 	 * @param assessmentPlan
 	 * @param sort
 	 * @param signalActionList
+	 * @param actions
+	 */
+	private void createAssessmentActions(AssessmentPlan assessmentPlan, Sort sort, List<SignalAction> signalActionList, List<SignalAction> actions) {
+		if (!CollectionUtils.isEmpty(actions)) {
+			for (SignalAction action : actions) {
+				createAssessmentAction(assessmentPlan, sort,signalActionList, action);
+			}
+		}
+	}
+
+	/**
+	 * @param assessmentPlan
+	 * @param sort
+	 * @param signalActionList
 	 * @param action
 	 */
-	private void createAssessmentAction(AssessmentPlan assessmentPlan,
-			Sort sort, List<SignalAction> signalActionList, SignalAction action) {
+	private void createAssessmentAction(AssessmentPlan assessmentPlan, Sort sort, List<SignalAction> signalActionList, SignalAction action) {
 		SignalAction signalAction = new SignalAction();
 		signalAction.setCaseInstanceId(assessmentPlan.getCaseInstanceId());
 		signalAction.setActionName(action.getActionName());
