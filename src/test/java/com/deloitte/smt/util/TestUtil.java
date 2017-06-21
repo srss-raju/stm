9536cb47150c98fb9ad5083760739399bd6a2053
RajeshKumar B <rbongurala@innominds.com>
2017-06-21 16:50:30 +0530

    testUpdate method is added

--- src/test/java/com/deloitte/smt/util/TestUtil.java
+++ src/test/java/com/deloitte/smt/util/TestUtil.java
@@ -1,7 +1,10 @@
 package com.deloitte.smt.util;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.deloitte.smt.entity.AssessmentActionType;
 import com.deloitte.smt.entity.SignalAction;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -23,6 +26,15 @@ public class TestUtil {
     	signalAction.setInDays(5);
     	signalAction.setCaseInstanceId("1111");
     	return signalAction;
-    	
     }
+    
+	public static List<AssessmentActionType> buildAssessmentActionTypes() {
+		List<AssessmentActionType> assessmentActionTypes = new ArrayList<>();
+		AssessmentActionType assessmentActionType = new AssessmentActionType();
+		assessmentActionType.setId(101l);
+		assessmentActionType.setName("Type 1");
+		assessmentActionType.setDescription("Type 1 Description");
+		assessmentActionTypes.add(assessmentActionType);
+		return assessmentActionTypes;
+	}
 }
