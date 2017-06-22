26179e1df73724a4732a19de8e6b5feb15daa363
RajeshKumar B <rbongurala@innominds.com>
2017-06-22 17:54:28 +0530

    added testUpdateAssessment

--- src/test/java/com/deloitte/smt/servicetest/AssessmentPlanServiceTest.java
+++ src/test/java/com/deloitte/smt/servicetest/AssessmentPlanServiceTest.java
@@ -2,7 +2,9 @@ package com.deloitte.smt.servicetest;
 
 import static org.mockito.BDDMockito.given;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.apache.log4j.Logger;
@@ -23,6 +25,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.deloitte.smt.SignalManagementApplication;
 import com.deloitte.smt.entity.AssessmentPlan;
+import com.deloitte.smt.entity.Comments;
+import com.deloitte.smt.entity.SignalURL;
 import com.deloitte.smt.entity.Topic;
 import com.deloitte.smt.repository.AssessmentPlanRepository;
 import com.deloitte.smt.service.AssessmentPlanService;
@@ -101,6 +105,32 @@ public class AssessmentPlanServiceTest {
 		assessmentPlan.setAssessmentName("Test Assessment 1");
 		given(this.assessmentPlanRepository.findOne(121l)).willReturn(assessmentPlan);
 		assessmentPlanService.unlinkSignalToAssessment(121l,100l);
+	}
+	
+	@Test
+	public void testUpdateAssessment() throws Exception{
+		AssessmentPlan assessmentPlan = new AssessmentPlan();
+		Set<Topic> topics = new HashSet<>();
+		Topic topic = new Topic();
+		topic.setId(100l);
+		topic.setName("Test Topic 1");
+		topics.add(topic);
+		assessmentPlan.setTopics(topics);
+		assessmentPlan.setAssessmentName("Test Assessment 1");
+		List<Comments> list = new ArrayList<>();
+		Comments comments = new Comments();
+		comments.setId(1l);
+		comments.setUserComments("Test Comments");
+		list.add(comments);
+		List<SignalURL> urls = new ArrayList<>();
+		SignalURL url = new SignalURL();
+		url.setUrl("www.test.com");
+		url.setDescription("test description");
+		urls.add(url);
+		assessmentPlan.setSignalUrls(urls);
+		assessmentPlan.setComments(list);
+		assessmentPlan.setId(11l);
+		assessmentPlanService.updateAssessment(assessmentPlan, null);
 	}
 
 }
