b8fa6b15b1bbf3a84c5a17e111947260ecb5339f
RajeshKumar B <rbongurala@innominds.com>
2017-06-26 01:12:21 +0530

    unused import removed

--- src/test/java/com/deloitte/smt/servicetest/AttachmentServiceTest.java
+++ src/test/java/com/deloitte/smt/servicetest/AttachmentServiceTest.java
@@ -15,7 +15,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
