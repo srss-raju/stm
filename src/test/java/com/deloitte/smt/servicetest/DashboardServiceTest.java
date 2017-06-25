d0fec2fe90b55830dc5ca0274dc163eb77509114
RajeshKumar B <rbongurala@innominds.com>
2017-06-26 01:43:04 +0530

    DashboardServiceTest is added

---
+++ src/test/java/com/deloitte/smt/servicetest/DashboardServiceTest.java
@@ -0,0 +1,68 @@
+package com.deloitte.smt.servicetest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import org.apache.log4j.Logger;
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngineConfiguration;
+import org.camunda.bpm.engine.impl.cfg.StandaloneInMemProcessEngineConfiguration;
+import org.camunda.bpm.engine.test.ProcessEngineRule;
+import org.camunda.bpm.engine.test.mock.MockExpressionManager;
+import org.junit.AfterClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.deloitte.smt.SignalManagementApplication;
+import com.deloitte.smt.repository.TopicRepository;
+import com.deloitte.smt.service.DashboardService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=SignalManagementApplication.class)
+@TestPropertySource(locations = {"classpath:test.properties"})
+public class DashboardServiceTest {
+	
+	private static final Logger LOG = Logger.getLogger(DashboardServiceTest.class);
+	
+	@Autowired
+	private DashboardService dashboardService;
+	
+	@PersistenceContext
+	private EntityManager entityManager;
+	
+	@MockBean
+	TopicRepository topicRepository;
+	
+		
+	private static final ProcessEngineConfiguration processEngineConfiguration = new StandaloneInMemProcessEngineConfiguration() {
+	    {
+	      jobExecutorActivate = false;
+	      expressionManager = new MockExpressionManager();
+	      databaseSchemaUpdate = DB_SCHEMA_UPDATE_CREATE_DROP;
+	    }
+	  };
+	  
+	  private static final ProcessEngine PROCESS_ENGINE_NEEDS_CLOSE = processEngineConfiguration.buildProcessEngine();
+	  
+	  @Rule
+	  public final ProcessEngineRule processEngine = new ProcessEngineRule(PROCESS_ENGINE_NEEDS_CLOSE);
+
+	  @AfterClass
+	  public static void shutdown() {
+	    PROCESS_ENGINE_NEEDS_CLOSE.close();
+	  }
+	  
+	  @Test
+		public void testGetSmtComplianceDetails() throws Exception{
+		  LOG.info("testGetSmtComplianceDetails");
+		  dashboardService.getSmtComplianceDetails();
+		}
+
+	
+}
