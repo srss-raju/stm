052a33bf81332e00d4a7ebfe1fa36b9bc8f0e2e1
RajeshKumar B <rbongurala@innominds.com>
2017-06-30 12:37:53 +0530

    SearchServiceTest is added

--- src/test/java/com/deloitte/smt/servicetest/SearchServiceTest.java
+++ src/test/java/com/deloitte/smt/servicetest/SearchServiceTest.java
@@ -1,5 +1,12 @@
 package com.deloitte.smt.servicetest;
 
+import static org.mockito.BDDMockito.given;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
 import org.camunda.bpm.engine.impl.cfg.StandaloneInMemProcessEngineConfiguration;
@@ -16,6 +23,10 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.deloitte.smt.SignalManagementApplication;
+import com.deloitte.smt.dto.SearchDto;
+import com.deloitte.smt.entity.Ingredient;
+import com.deloitte.smt.entity.License;
+import com.deloitte.smt.entity.Product;
 import com.deloitte.smt.repository.AssessmentPlanRepository;
 import com.deloitte.smt.repository.HlgtRepository;
 import com.deloitte.smt.repository.HltRepository;
@@ -81,6 +92,80 @@ public class SearchServiceTest {
 	@Test
 	public void testGetFiltersForSignal() {
 		searchService.getFiltersForSignal();
+	}
+	
+	@Test
+	public void testGetFiltersForSignalDetection() {
+		searchService.getFiltersForSignalDetection();
+	}
+	
+	@Test
+	public void testGetAllFiltersForAssessmentPlan() {
+		Set<Long> topicIds = new HashSet<>();
+		topicIds.add(1l);
+		given(this.assessmentPlanRepository.findAllSignalIds()).willReturn(topicIds);
+		searchService.getAllFiltersForAssessmentPlan();
+	}
+	
+	@Test
+	public void testGetAllFiltersForRiskPlan() {
+		Set<Long> topicIds = new HashSet<>();
+		topicIds.add(1l);
+		given(this.riskPlanRepository.findAllSignalIds()).willReturn(topicIds);
+		searchService.getAllFiltersForRiskPlan();
+	}
+	
+	@Test
+	public void testGetIngredients() {
+		searchService.getIngredients();
+	}
+	
+	@Test
+	public void testGetSignalIdsForSearchWithLicense() {
+		SearchDto searchDto = new SearchDto();
+		List<String> licenses = new ArrayList<>();
+		licenses.add("Test License");
+		searchDto.setLicenses(licenses);
+		List<Long> topicIds = new ArrayList<>();
+		topicIds.add(1l);
+		List<License> licenseList = new ArrayList<>();
+		License license = new License();
+		license.setTopicId(1l);
+		licenseList.add(license);
+		given(this.licenseRepository.findAllByLicenseNameIn(licenses)).willReturn(licenseList);
+		searchService.getSignalIdsForSearch(searchDto, topicIds, false);
+	}
+	
+	@Test
+	public void testGetSignalIdsForSearchWithIngredient() {
+		SearchDto searchDto = new SearchDto();
+		List<String> ingredients = new ArrayList<>();
+		ingredients.add("Test Ingredient");
+		searchDto.setIngredients(ingredients);
+		List<Long> topicIds = new ArrayList<>();
+		topicIds.add(1l);
+		List<Ingredient> ingredientList = new ArrayList<>();
+		Ingredient ingredient = new Ingredient();
+		ingredient.setTopicId(1l);
+		ingredientList.add(ingredient);
+		given(this.ingredientRepository.findAllByIngredientNameIn(ingredients)).willReturn(ingredientList);
+		searchService.getSignalIdsForSearch(searchDto, topicIds, false);
+	}
+	
+	@Test
+	public void testGetSignalIdsForSearchWithProduct() {
+		SearchDto searchDto = new SearchDto();
+		List<String> products = new ArrayList<>();
+		products.add("Test Product");
+		searchDto.setProducts(products);
+		List<Long> topicIds = new ArrayList<>();
+		topicIds.add(1l);
+		List<Product> productList = new ArrayList<>();
+		Product product = new Product();
+		product.setTopicId(1l);
+		productList.add(product);
+		given(this.productRepository.findAllByProductNameIn(products)).willReturn(productList);
+		searchService.getSignalIdsForSearch(searchDto, topicIds, false);
 	}
 	
 }
