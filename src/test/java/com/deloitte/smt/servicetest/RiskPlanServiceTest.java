f668c5c337fe2840b99037f0163c7978922fd657
RajeshKumar B <rbongurala@innominds.com>
2017-06-30 18:31:25 +0530

    RiskPlanServiceTest is added

--- src/test/java/com/deloitte/smt/servicetest/RiskPlanServiceTest.java
+++ src/test/java/com/deloitte/smt/servicetest/RiskPlanServiceTest.java
@@ -3,6 +3,7 @@ package com.deloitte.smt.servicetest;
 import static org.mockito.BDDMockito.given;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -27,9 +28,13 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.deloitte.smt.SignalManagementApplication;
+import com.deloitte.smt.constant.DateKeyType;
+import com.deloitte.smt.dto.SearchDto;
 import com.deloitte.smt.entity.AssessmentPlan;
 import com.deloitte.smt.entity.AssignmentConfiguration;
+import com.deloitte.smt.entity.Comments;
 import com.deloitte.smt.entity.RiskPlan;
+import com.deloitte.smt.entity.RiskTask;
 import com.deloitte.smt.entity.SignalURL;
 import com.deloitte.smt.repository.AssessmentPlanRepository;
 import com.deloitte.smt.repository.AssignmentConfigurationRepository;
@@ -89,7 +94,7 @@ public class RiskPlanServiceTest {
 	@MockBean
 	IngredientRepository ingredientRepository;
 
-	@Autowired
+	@MockBean
 	SearchService searchService;
 
 	@PersistenceContext
@@ -203,5 +208,262 @@ public class RiskPlanServiceTest {
 		}catch(Exception ex){
 			LOG.info(ex);
 		}
+	}
+	
+	@Test
+	public void testFindAllRiskPlansForSearch2DueDate() {
+		try{
+			SearchDto searchDto = new SearchDto();
+			getSearchDto(searchDto);
+			searchDto.setDateKey(DateKeyType.DUEDATE.name());
+			riskPlanService.findAllRiskPlansForSearch2(searchDto);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+
+	@Test
+	public void testFindAllRiskPlansForSearch2CreatedDate() {
+		try{
+			SearchDto searchDto = new SearchDto();
+			getSearchDto(searchDto);
+			searchDto.setDateKey(DateKeyType.CREATED.name());
+			riskPlanService.findAllRiskPlansForSearch2(searchDto);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testFindAllRiskPlansForSearchAllFalse() {
+		try{
+			SearchDto searchDto = new SearchDto();
+			getSearchDto(searchDto);
+			searchDto.setDateKey(DateKeyType.CREATED.name());
+			List<Long> riskTopicIds = new ArrayList<>();
+			riskTopicIds.add(1l);
+			given(this.searchService.getSignalIdsForSearch(searchDto, riskTopicIds, true)).willReturn(true);
+			riskPlanService.findAllRiskPlansForSearch(searchDto);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testCreateRiskTask() {
+		try{
+			RiskPlan riskPlan = new RiskPlan();
+			RiskTask riskTask = new RiskTask();
+			riskTask.setCaseInstanceId("11");
+			riskTask.setStatus("Completed1");
+			riskTask.setId(1l);
+			riskTask.setRiskId("1");
+			given(this. riskPlanRepository.findOne(1l)).willReturn(riskPlan);
+			RiskTask riskTaskUpdated = new RiskTask();
+			List<SignalURL> urls = new ArrayList<>();
+			SignalURL url = new SignalURL();
+			urls.add(url);
+			riskTaskUpdated.setSignalUrls(urls);
+			given(this. riskTaskRepository.save(riskTask)).willReturn(riskTaskUpdated);
+			riskPlanService.createRiskTask(riskTask, null);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testFindById() {
+		try{
+			RiskTask riskTask = new RiskTask();
+			riskTask.setId(1l);
+			riskTask.setStatus("New");
+			given(this.riskTaskRepository.findOne(1l)).willReturn(riskTask);
+			given(this.riskTaskRepository.save(riskTask)).willReturn(riskTask);
+			riskPlanService.findById(1l);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testFindAllByRiskIdStatusNull() {
+		try{
+			List<RiskTask> list = new ArrayList<>();
+			given(this.riskTaskRepository.findAllByRiskIdOrderByCreatedDateDesc("1")).willReturn(list);
+			riskPlanService.findAllByRiskId("1", null);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testFindAllByRiskIdStatus() {
+		try{
+			List<RiskTask> list = new ArrayList<>();
+			given(this.riskTaskRepository.findAllByRiskIdAndStatusOrderByCreatedDateDesc("1", "Completed")).willReturn(list);
+			riskPlanService.findAllByRiskId("1", "Completed");
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testDeleteWithNull() {
+		try{
+			RiskTask riskTask = new RiskTask();
+			given(this.riskTaskRepository.findOne(1l)).willReturn(riskTask);
+			riskPlanService.delete(1l, "1");
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testDelete() {
+		try{
+			riskPlanService.delete(1l, "1");
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testUpdateRiskTask() {
+		try{
+			RiskTask riskTask = new RiskTask();
+			riskTask.setCaseInstanceId("11");
+			riskTask.setStatus("Completed1");
+			riskTask.setId(1l);
+			riskTask.setRiskId("1");
+			List<SignalURL> urls = new ArrayList<>();
+			SignalURL url = new SignalURL();
+			urls.add(url);
+			riskTask.setSignalUrls(urls);
+			riskPlanService.updateRiskTask(riskTask, null);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testUpdateRiskTaskClosed() {
+		try{
+			RiskTask riskTask = new RiskTask();
+			riskTask.setCaseInstanceId("11");
+			riskTask.setStatus("Completed1");
+			riskTask.setId(1l);
+			riskTask.setRiskId("1");
+			List<SignalURL> urls = new ArrayList<>();
+			SignalURL url = new SignalURL();
+			urls.add(url);
+			riskTask.setSignalUrls(urls);
+			List<RiskTask> list = new ArrayList<>();
+			list.add(riskTask);
+			given(this.riskTaskRepository.findAllByRiskIdOrderByCreatedDateDesc("1")).willReturn(list);
+			riskPlanService.updateRiskTask(riskTask, null);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testFindByRiskId() {
+		try{
+			RiskPlan riskPlan = new RiskPlan();
+			riskPlan.setId(1l);
+			riskPlan.setStatus("New");
+			given(this.riskPlanRepository.findOne(1l)).willReturn(riskPlan);
+			given(this.riskPlanRepository.save(riskPlan)).willReturn(riskPlan);
+			riskPlanService.findByRiskId(1l);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testFindByRiskIdForNull() {
+		try{
+			riskPlanService.findByRiskId(1l);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testRiskPlanSummary() {
+		try{
+			RiskPlan riskPlan = new RiskPlan();
+			riskPlan.setId(1l);
+			riskPlanService.riskPlanSummary(riskPlan, null);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testRiskPlanSummaryWithNull() {
+		try{
+			RiskPlan riskPlan = new RiskPlan();
+			riskPlanService.riskPlanSummary(riskPlan, null);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testUpdateRiskPlan() {
+		try{
+			RiskPlan riskPlan = new RiskPlan();
+			riskPlan.setId(1l);
+			List<Comments> list = new ArrayList<>();
+			Comments comments = new Comments();
+			comments.setUserComments("Test Comments");
+			list.add(comments);
+			riskPlan.setComments(list);
+			List<SignalURL> urls = new ArrayList<>();
+			SignalURL url = new SignalURL();
+			urls.add(url);
+			riskPlan.setSignalUrls(urls);
+			riskPlanService.updateRiskPlan(riskPlan, null);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	
+	@Test
+	public void testUpdateRiskPlanWithNull() {
+		try{
+			RiskPlan riskPlan = new RiskPlan();
+			riskPlanService.updateRiskPlan(riskPlan, null);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	private void getSearchDto(SearchDto searchDto) {
+		List<String> ingredients = new ArrayList<>();
+		ingredients.add("Test Ingredient");
+		List<String> licenses = new ArrayList<>();
+		licenses.add("Test Licenses");
+		List<String> products = new ArrayList<>();
+		products.add("Test Products");
+		List<String> statuses = new ArrayList<>();
+		statuses.add("Test Statuses");
+		List<String> assignees = new ArrayList<>();
+		assignees.add("Test Assignees");
+		List<String> riskTaskStatus = new ArrayList<>();
+		riskTaskStatus.add("Test RiskTaskStatus");
+		Date startDate = new Date();
+		Date endDate = new Date();
+		
+		searchDto.setIngredients(ingredients);
+		searchDto.setLicenses(licenses);
+		searchDto.setProducts(products);
+		searchDto.setStatuses(statuses);
+		searchDto.setAssignees(assignees);
+		searchDto.setRiskTaskStatus(riskTaskStatus);
+		searchDto.setStartDate(startDate);
+		searchDto.setEndDate(endDate);
 	}
 }
