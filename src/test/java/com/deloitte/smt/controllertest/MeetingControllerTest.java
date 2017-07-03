c558a2271c8d7a62f7956f10c4e394e2b3d2268b
chandra sekhar avula <cavula@innominds.com>
2017-07-03 14:30:33 +0530

    MeetingControllerTest

--- src/test/java/com/deloitte/smt/controllertest/MeetingControllerTest.java
+++ src/test/java/com/deloitte/smt/controllertest/MeetingControllerTest.java
@@ -6,35 +6,22 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.Matchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.deloitte.smt.SignalManagementApplication;
 import com.deloitte.smt.dto.SearchDto;
-import com.deloitte.smt.entity.AssessmentPlan;
-import com.deloitte.smt.entity.Topic;
-import com.deloitte.smt.service.AssessmentPlanService;
 import com.deloitte.smt.service.SearchService;
-import com.deloitte.smt.util.TestUtil;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
-
-import static org.mockito.Matchers.anyLong;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 
 @RunWith(SpringRunner.class)
@@ -78,5 +65,54 @@ public class MeetingControllerTest {
 				.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
 	}
 	
+	
+	@Test
+	public void testGetFiltersForSignalDetection()throws Exception{
+		SearchDto searchDto=new SearchDto();
+		when(searchService.getFiltersForSignalDetection()).thenReturn(searchDto);
+		this.mockMvc
+				.perform(get("/camunda/api/signal/utils/filters/signalDetection")
+						.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+	}
+	
+	@Test
+	public void testGetFiltersForAssessmentPlan()throws Exception{
+		SearchDto searchDto=new SearchDto();
+		when(searchService.getAllFiltersForAssessmentPlan()).thenReturn(searchDto);
+		this.mockMvc
+				.perform(get("/camunda/api/signal/utils/filters/assessmentPlan")
+						.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+	}
+	
+	@Test
+	public void testGetFiltersForRiskPlan()throws Exception{
+		SearchDto searchDto=new SearchDto();
+		when(searchService.getAllFiltersForRiskPlan()).thenReturn(searchDto);
+		this.mockMvc
+				.perform(get("/camunda/api/signal/utils/filters/riskPlan")
+						.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+	}
+	
+	@Test
+	public void testGetIngredientFilters()throws Exception{
+		List<String> values=new ArrayList();
+		
+		when(searchService.getIngredients()).thenReturn(values);
+		this.mockMvc
+				.perform(get("/camunda/api/signal/utils/filters/ingredients")
+						.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+	}
+	
+	@Test
+	public void testGetSeverityFilters()throws Exception{
+		this.mockMvc
+				.perform(get("/camunda/api/signal/utils/filters/severity")
+						.contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(status().isOk()).andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE));
+	}
 	
 }
