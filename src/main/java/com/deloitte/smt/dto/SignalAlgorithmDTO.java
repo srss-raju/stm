9dee0977922b07e26909c18e812811f86121e374
ShashikalaB <shbondada@innominds.com>
2017-08-08 16:25:27 +0530

    Exporting excel dto class

---
+++ src/main/java/com/deloitte/smt/dto/SignalAlgorithmDTO.java
@@ -0,0 +1,341 @@
+package com.deloitte.smt.dto;
+
+import java.io.Serializable;
+
+public class SignalAlgorithmDTO implements Serializable {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6678141117765723101L;
+	
+	private String familyDescription;
+	
+	private String socDescription;
+	
+	private String ptDescription;
+	
+	private String cases;
+	
+	private String outcomeDE;
+	
+	private String outcomeHO;
+	
+	private String outcomeLT;
+	
+	private String outcomeCA;
+	
+	private String outcomeRI;
+	
+	private String outcomeOT;
+	
+	private String algorithmPRRScore;
+	
+	private String algorithmPRRLB;
+	
+	private String algorithmPRRUB;
+	
+	private String algorithmPRRSQ;
+	
+	private String algorithmPRRSTDev;
+	
+	private String algorithmRORScore;
+	
+	private String algorithmRORSTDev;
+	
+	private String algorithmRORLB;
+	
+	private String algorithmRORUB;
+	
+	private String algorithmRRRScore;
+	
+	private String algorithmRRRSTDev;
+	
+	private String algorithmRRRLB;
+	
+	private String algorithmRRRUB;
+	
+	private String algorithmRRRInfoComp;
+	
+	private String algorithmEBScore;
+	
+	private String algorithmEBLB;
+	
+	private String algorithmEBUB;
+	
+	private String algorithmBCPNNScore;
+	
+	private String algorithmBCPNNLB;
+	
+	private String algorithmBCPNNUB;
+	
+	private String pp;
+	
+	private String signal;
+	
+	private String signalId;
+
+	public String getFamilyDescription() {
+		return familyDescription;
+	}
+
+	public void setFamilyDescription(String familyDescription) {
+		this.familyDescription = familyDescription;
+	}
+
+	public String getSocDescription() {
+		return socDescription;
+	}
+
+	public void setSocDescription(String socDescription) {
+		this.socDescription = socDescription;
+	}
+
+	public String getPtDescription() {
+		return ptDescription;
+	}
+
+	public void setPtDescription(String ptDescription) {
+		this.ptDescription = ptDescription;
+	}
+
+	public String getCases() {
+		return cases;
+	}
+
+	public void setCases(String cases) {
+		this.cases = cases;
+	}
+
+	public String getOutcomeDE() {
+		return outcomeDE;
+	}
+
+	public void setOutcomeDE(String outcomeDE) {
+		this.outcomeDE = outcomeDE;
+	}
+
+	public String getOutcomeHO() {
+		return outcomeHO;
+	}
+
+	public void setOutcomeHO(String outcomeHO) {
+		this.outcomeHO = outcomeHO;
+	}
+
+	public String getOutcomeLT() {
+		return outcomeLT;
+	}
+
+	public void setOutcomeLT(String outcomeLT) {
+		this.outcomeLT = outcomeLT;
+	}
+
+	public String getOutcomeCA() {
+		return outcomeCA;
+	}
+
+	public void setOutcomeCA(String outcomeCA) {
+		this.outcomeCA = outcomeCA;
+	}
+
+	public String getOutcomeRI() {
+		return outcomeRI;
+	}
+
+	public void setOutcomeRI(String outcomeRI) {
+		this.outcomeRI = outcomeRI;
+	}
+
+	public String getOutcomeOT() {
+		return outcomeOT;
+	}
+
+	public void setOutcomeOT(String outcomeOT) {
+		this.outcomeOT = outcomeOT;
+	}
+
+	public String getAlgorithmPRRScore() {
+		return algorithmPRRScore;
+	}
+
+	public void setAlgorithmPRRScore(String algorithmPRRScore) {
+		this.algorithmPRRScore = algorithmPRRScore;
+	}
+
+	public String getAlgorithmPRRLB() {
+		return algorithmPRRLB;
+	}
+
+	public void setAlgorithmPRRLB(String algorithmPRRLB) {
+		this.algorithmPRRLB = algorithmPRRLB;
+	}
+
+	public String getAlgorithmPRRUB() {
+		return algorithmPRRUB;
+	}
+
+	public void setAlgorithmPRRUB(String algorithmPRRUB) {
+		this.algorithmPRRUB = algorithmPRRUB;
+	}
+
+	public String getAlgorithmPRRSQ() {
+		return algorithmPRRSQ;
+	}
+
+	public void setAlgorithmPRRSQ(String algorithmPRRSQ) {
+		this.algorithmPRRSQ = algorithmPRRSQ;
+	}
+
+	public String getAlgorithmPRRSTDev() {
+		return algorithmPRRSTDev;
+	}
+
+	public void setAlgorithmPRRSTDev(String algorithmPRRSTDev) {
+		this.algorithmPRRSTDev = algorithmPRRSTDev;
+	}
+
+	public String getAlgorithmRORScore() {
+		return algorithmRORScore;
+	}
+
+	public void setAlgorithmRORScore(String algorithmRORScore) {
+		this.algorithmRORScore = algorithmRORScore;
+	}
+
+	public String getAlgorithmRORSTDev() {
+		return algorithmRORSTDev;
+	}
+
+	public void setAlgorithmRORSTDev(String algorithmRORSTDev) {
+		this.algorithmRORSTDev = algorithmRORSTDev;
+	}
+
+	public String getAlgorithmRORLB() {
+		return algorithmRORLB;
+	}
+
+	public void setAlgorithmRORLB(String algorithmRORLB) {
+		this.algorithmRORLB = algorithmRORLB;
+	}
+
+	public String getAlgorithmRORUB() {
+		return algorithmRORUB;
+	}
+
+	public void setAlgorithmRORUB(String algorithmRORUB) {
+		this.algorithmRORUB = algorithmRORUB;
+	}
+
+	public String getAlgorithmRRRScore() {
+		return algorithmRRRScore;
+	}
+
+	public void setAlgorithmRRRScore(String algorithmRRRScore) {
+		this.algorithmRRRScore = algorithmRRRScore;
+	}
+
+	public String getAlgorithmRRRSTDev() {
+		return algorithmRRRSTDev;
+	}
+
+	public void setAlgorithmRRRSTDev(String algorithmRRRSTDev) {
+		this.algorithmRRRSTDev = algorithmRRRSTDev;
+	}
+
+	public String getAlgorithmRRRLB() {
+		return algorithmRRRLB;
+	}
+
+	public void setAlgorithmRRRLB(String algorithmRRRLB) {
+		this.algorithmRRRLB = algorithmRRRLB;
+	}
+
+	public String getAlgorithmRRRUB() {
+		return algorithmRRRUB;
+	}
+
+	public void setAlgorithmRRRUB(String algorithmRRRUB) {
+		this.algorithmRRRUB = algorithmRRRUB;
+	}
+
+	public String getAlgorithmRRRInfoComp() {
+		return algorithmRRRInfoComp;
+	}
+
+	public void setAlgorithmRRRInfoComp(String algorithmRRRInfoComp) {
+		this.algorithmRRRInfoComp = algorithmRRRInfoComp;
+	}
+
+	public String getAlgorithmEBScore() {
+		return algorithmEBScore;
+	}
+
+	public void setAlgorithmEBScore(String algorithmEBScore) {
+		this.algorithmEBScore = algorithmEBScore;
+	}
+
+	public String getAlgorithmEBLB() {
+		return algorithmEBLB;
+	}
+
+	public void setAlgorithmEBLB(String algorithmEBLB) {
+		this.algorithmEBLB = algorithmEBLB;
+	}
+
+	public String getAlgorithmEBUB() {
+		return algorithmEBUB;
+	}
+
+	public void setAlgorithmEBUB(String algorithmEBUB) {
+		this.algorithmEBUB = algorithmEBUB;
+	}
+
+	public String getAlgorithmBCPNNScore() {
+		return algorithmBCPNNScore;
+	}
+
+	public void setAlgorithmBCPNNScore(String algorithmBCPNNScore) {
+		this.algorithmBCPNNScore = algorithmBCPNNScore;
+	}
+
+	public String getAlgorithmBCPNNLB() {
+		return algorithmBCPNNLB;
+	}
+
+	public void setAlgorithmBCPNNLB(String algorithmBCPNNLB) {
+		this.algorithmBCPNNLB = algorithmBCPNNLB;
+	}
+
+	public String getAlgorithmBCPNNUB() {
+		return algorithmBCPNNUB;
+	}
+
+	public void setAlgorithmBCPNNUB(String algorithmBCPNNUB) {
+		this.algorithmBCPNNUB = algorithmBCPNNUB;
+	}
+
+	public String getPp() {
+		return pp;
+	}
+
+	public void setPp(String pp) {
+		this.pp = pp;
+	}
+
+	public String getSignal() {
+		return signal;
+	}
+
+	public void setSignal(String signal) {
+		this.signal = signal;
+	}
+
+	public String getSignalId() {
+		return signalId;
+	}
+
+	public void setSignalId(String signalId) {
+		this.signalId = signalId;
+	}
+}
