b0a7eb73f2d7ba45ed2b7124498215267eb51398
shashikala <shbondada@innominds.com>
2017-08-11 11:53:23 +0530

    Modified the controller to accept UI response to generate excel sheet

--- src/main/java/com/deloitte/smt/controller/ExportToExcelController.java
+++ src/main/java/com/deloitte/smt/controller/ExportToExcelController.java
@@ -11,28 +11,42 @@ import org.apache.log4j.Logger;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.deloitte.smt.dto.SignalAlgorithmDTO;
 import com.deloitte.smt.service.ExportExcelService;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @RestController
 @RequestMapping("/camunda/api/signal")
 public class ExportToExcelController {
-	private static final Logger LOG = Logger.getLogger(ExportToExcelController.class);
-	
+	private static final Logger LOG = Logger
+			.getLogger(ExportToExcelController.class);
+
 	@Autowired
 	ExportExcelService exportExcelService;
-	
-	private static final String EXPORT_EXCEL="ExportDetectionDetails.xls";
-	
+
+	private static final String EXPORT_EXCEL = "ExportDetectionDetails.xls";
+
 	@PostMapping(value = "/exportExcel")
-	public void generateExcel(@RequestBody List<SignalAlgorithmDTO> signalDTOList,HttpServletRequest request,HttpServletResponse response){
+	public void generateExcel(
+			@RequestParam(value = "data") String detectionDetails,
+			HttpServletRequest request, HttpServletResponse response)
+			throws JsonParseException, JsonMappingException, IOException {
+
+		List<SignalAlgorithmDTO> signalAlgorithmDtoList = new ObjectMapper()
+				.readValue(detectionDetails,
+						new TypeReference<List<SignalAlgorithmDTO>>() {
+						});
 		try {
-			HSSFWorkbook workbook  = exportExcelService.writeExcel(signalDTOList, EXPORT_EXCEL);
-			response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+			HSSFWorkbook workbook = exportExcelService.writeExcel(
+					signalAlgorithmDtoList, EXPORT_EXCEL);
+			response.setContentType("application/vnd.ms-excel");
 
 			response.setHeader("Content-disposition", "attachment; filename="
 					+ EXPORT_EXCEL);
@@ -40,9 +54,10 @@ public class ExportToExcelController {
 			workbook.write(out);
 			out.flush();
 			out.close();
+			// response.flushBuffer();
 
 		} catch (IOException e) {
-			LOG.info("Exception occured while exporting excel sheet "+e);
+			LOG.info("Exception occured while exporting excel sheet " + e);
 		}
 	}
 
