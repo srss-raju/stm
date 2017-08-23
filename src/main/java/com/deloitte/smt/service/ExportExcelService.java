372c60ab464200f57c02dafc33c1b8cd86401f7e
shashikala <shbondada@innominds.com>
2017-08-23 11:57:38 +0530

    "Added the Outcome in export excel"

--- src/main/java/com/deloitte/smt/service/ExportExcelService.java
+++ src/main/java/com/deloitte/smt/service/ExportExcelService.java
@@ -87,8 +87,9 @@ public class ExportExcelService {
 		sheet.setColumnWidth(1, 15000);
 		sheet.setColumnWidth(2, 5000);
 		sheet.setColumnWidth(4, 3500);
-		sheet.setColumnWidth(7, 3500);
 		sheet.setColumnWidth(10, 3500);
+		sheet.setColumnWidth(13, 3500);
+		sheet.setColumnWidth(16, 3500);
 		
 		workbook.createCellStyle();
 		/* Create HSSFFont object from the workbook */
@@ -117,70 +118,97 @@ public class ExportExcelService {
 		Cell cell3 = header.createCell(3);
 		cell3.setCellStyle(style);
 		cell3.setCellValue("CASES");
+		
+		Cell cell4 = header.createCell(4);
+		cell4.setCellStyle(style);
+		cell4.setCellValue("OUTCOME");
 
-		Cell cell10 = header.createCell(4);
+		Cell cell10 = header.createCell(10);
 		cell10.setCellStyle(style);
 		cell10.setCellValue("ALGORITHMS");
 
-		Cell cell21 = header.createCell(15);
+		Cell cell21 = header.createCell(21);
 		cell21.setCellStyle(style);
 		cell21.setCellValue("PP");
 
-		Cell cell22 = header.createCell(16);
+		Cell cell22 = header.createCell(22);
 		cell22.setCellStyle(style);
 		cell22.setCellValue("SIGNAL");
 
 		Row header2 = sheet.createRow(1);
-		Cell row2cell10 = header2.createCell(4);
+		
+		Cell row2cell10 = header2.createCell(10);
 		row2cell10.setCellStyle(style);
 		row2cell10.setCellValue("PRR");
 
-		Cell row2cell13 = header2.createCell(7);
+		Cell row2cell13 = header2.createCell(13);
 		row2cell13.setCellStyle(style);
 		row2cell13.setCellValue("ROR");
 
-		Cell row2cell16 = header2.createCell(10);
+		Cell row2cell16 = header2.createCell(16);
 		row2cell16.setCellStyle(style);
 		row2cell16.setCellValue("RRR");
 
-		Cell row2cell19 = header2.createCell(13);
+		Cell row2cell19 = header2.createCell(19);
 		row2cell19.setCellStyle(style);
 		row2cell19.setCellValue("EB05");
 
-		Cell row2cell20 = header2.createCell(14);
+		Cell row2cell20 = header2.createCell(20);
 		row2cell20.setCellStyle(style);
 		row2cell20.setCellValue("BCPNN");
 
 		Row header3 = sheet.createRow(2);
 
+		Cell row3cell4 = header3.createCell(4);
+		row3cell4.setCellStyle(style);
+		row3cell4.setCellValue("DE");
+		Cell row3cell5 = header3.createCell(5);
+		row3cell5.setCellStyle(style);
+		row3cell5.setCellValue("HO");
+		Cell row3cell6 = header3.createCell(6);
+		row3cell6.setCellStyle(style);
+		row3cell6.setCellValue("LT");
+
+		Cell row3cell7 = header3.createCell(7);
+		row3cell7.setCellStyle(style);
+		row3cell7.setCellValue("CA");
+		Cell row3cell8 = header3.createCell(8);
+		row3cell8.setCellStyle(style);
+		row3cell8.setCellValue("RI");
+		Cell row3cell9 = header3.createCell(9);
+		row3cell9.setCellStyle(style);
+		row3cell9.setCellValue("OT");
+
+		
+		
 		/** Algorithm attributes **/
-		Cell row3cell10 = header3.createCell(4);
+		Cell row3cell10 = header3.createCell(10);
 		row3cell10.setCellStyle(style);
 		row3cell10.setCellValue("PRRSCORE");
-		Cell row3cell11 = header3.createCell(5);
+		Cell row3cell11 = header3.createCell(11);
 		row3cell11.setCellStyle(style);
 		row3cell11.setCellValue("PRRLB");
-		Cell row3cell12 = header3.createCell(6);
+		Cell row3cell12 = header3.createCell(12);
 		row3cell12.setCellStyle(style);
 		row3cell12.setCellValue("PRRUB");
 
-		Cell row3cell13 = header3.createCell(7);
+		Cell row3cell13 = header3.createCell(13);
 		row3cell13.setCellStyle(style);
 		row3cell13.setCellValue("RORSCORE");
-		Cell row3cell14 = header3.createCell(8);
+		Cell row3cell14 = header3.createCell(14);
 		row3cell14.setCellStyle(style);
 		row3cell14.setCellValue("RORLB");
-		Cell row3cell15 = header3.createCell(9);
+		Cell row3cell15 = header3.createCell(15);
 		row3cell15.setCellStyle(style);
 		row3cell15.setCellValue("RORUB");
 
-		Cell row3cell16 = header3.createCell(10);
+		Cell row3cell16 = header3.createCell(16);
 		row3cell16.setCellStyle(style);
 		row3cell16.setCellValue("RRRSCORE");
-		Cell row3cell17 = header3.createCell(11);
+		Cell row3cell17 = header3.createCell(17);
 		row3cell17.setCellStyle(style);
 		row3cell17.setCellValue("RRRLB");
-		Cell row3cell18 = header3.createCell(12);
+		Cell row3cell18 = header3.createCell(18);
 		row3cell18.setCellStyle(style);
 		row3cell18.setCellValue("RRRUB");
 
@@ -211,38 +239,44 @@ public class ExportExcelService {
 		sheet.addMergedRegion(cellRangeAddress3);
 		cellRangeAdressList.add(cellRangeAddress3);
 
-		CellRangeAddress cellRangeAddress4 = new CellRangeAddress(0, 0, 4, 14);
+		CellRangeAddress cellRangeAddress4 = new CellRangeAddress(0, 1, 4, 9);
 		sheet.addMergedRegion(cellRangeAddress4);
 		cellRangeAdressList.add(cellRangeAddress4);
 
-		CellRangeAddress cellRangeAddress21 = new CellRangeAddress(0, 2, 15, 15);
-		sheet.addMergedRegion(cellRangeAddress21);
-		cellRangeAdressList.add(cellRangeAddress21);
 		
-		CellRangeAddress cellRangeAddress22 = new CellRangeAddress(0, 2, 16, 16);
-		sheet.addMergedRegion(cellRangeAddress22);
-		cellRangeAdressList.add(cellRangeAddress22);
-		
-		CellRangeAddress cellRangeAddress6 = new CellRangeAddress(1,1,4,6);
-		sheet.addMergedRegion(cellRangeAddress6);
-		cellRangeAdressList.add(cellRangeAddress6);
-		
-		CellRangeAddress cellRangeAddress7 = new CellRangeAddress(1,1,7,9);
-		sheet.addMergedRegion(cellRangeAddress7);
-		cellRangeAdressList.add(cellRangeAddress7);
-		
-		CellRangeAddress cellRangeAddress10 = new CellRangeAddress(1,1,10,12);
+		CellRangeAddress cellRangeAddress10 = new CellRangeAddress(0, 0, 10, 20);
 		sheet.addMergedRegion(cellRangeAddress10);
 		cellRangeAdressList.add(cellRangeAddress10);
 		
-		CellRangeAddress cellRangeAddress13 = new CellRangeAddress(1,2,13,13);
-		sheet.addMergedRegion(cellRangeAddress13);
-		cellRangeAdressList.add(cellRangeAddress13);
 		
-		CellRangeAddress cellRangeAddress14 = new CellRangeAddress(1,2,14,14);
+		CellRangeAddress cellRangeAddress19 = new CellRangeAddress(1, 2, 19, 19);
+		sheet.addMergedRegion(cellRangeAddress19);
+		cellRangeAdressList.add(cellRangeAddress19);
+		
+		CellRangeAddress cellRangeAddress20 = new CellRangeAddress(1, 2, 20, 20);
+		sheet.addMergedRegion(cellRangeAddress20);
+		cellRangeAdressList.add(cellRangeAddress20);
+		
+		CellRangeAddress cellRangeAddress21 = new CellRangeAddress(0, 2, 21, 21);
+		sheet.addMergedRegion(cellRangeAddress21);
+		cellRangeAdressList.add(cellRangeAddress21);
+		
+		CellRangeAddress cellRangeAddress22 = new CellRangeAddress(0, 2, 22, 22);
+		sheet.addMergedRegion(cellRangeAddress22);
+		cellRangeAdressList.add(cellRangeAddress22);
+		
+		CellRangeAddress cellRangeAddress12 = new CellRangeAddress(1,1,10,12);
+		sheet.addMergedRegion(cellRangeAddress12);
+		cellRangeAdressList.add(cellRangeAddress12);
+		
+		CellRangeAddress cellRangeAddress14 = new CellRangeAddress(1,1,13,15);
 		sheet.addMergedRegion(cellRangeAddress14);
 		cellRangeAdressList.add(cellRangeAddress14);
-		 
+		
+		CellRangeAddress cellRangeAddress17 = new CellRangeAddress(1,1,16,18);
+		sheet.addMergedRegion(cellRangeAddress17);
+		cellRangeAdressList.add(cellRangeAddress17);
+		
 		
 		for (CellRangeAddress rangeAddress : cellRangeAdressList) {
 		
@@ -271,76 +305,77 @@ public class ExportExcelService {
 			/* attach the font to the style created earlier */
 			style.setFont(my_font);
 		
-		int rowCount = 2;
-		for (SignalAlgorithmDTO signalDTO : signalDTOList) {
-
-			Row row = sheet.createRow(++rowCount);
-
+			int rowCount = 2;
+			for (SignalAlgorithmDTO signalDTO : signalDTOList) {
+				
+				Row row = sheet.createRow(++rowCount);
+			
 			Cell cell = row.createCell(0);
-			cell.setCellStyle(style);
 			cell.setCellValue(signalDTO.getFamilyDescription());
 
 			cell = row.createCell(1);
-			cell.setCellStyle(style);
 			cell.setCellValue(signalDTO.getSocDescription());
 
 			cell = row.createCell(2);
-			cell.setCellStyle(style);
 			cell.setCellValue(signalDTO.getPtDescription());
-
-			cell = row.createCell(3);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(3);
 			cell.setCellValue(signalDTO.getCases());
-
-			cell = row.createCell(4);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(4);
+			cell.setCellValue(signalDTO.getOutcomeDE());
+			
+			cell=row.createCell(5);
+			cell.setCellValue(signalDTO.getOutcomeHO());
+			
+			cell=row.createCell(6);
+			cell.setCellValue(signalDTO.getOutcomeLT());
+			
+			cell=row.createCell(7);
+			cell.setCellValue(signalDTO.getOutcomeCA());
+			
+			cell=row.createCell(8);
+			cell.setCellValue(signalDTO.getOutcomeRI());
+			
+			cell=row.createCell(9);
+			cell.setCellValue(signalDTO.getOutcomeOT());
+			
+			cell=row.createCell(10);
 			cell.setCellValue(signalDTO.getAlgorithmPRRScore());
-
-			cell = row.createCell(5);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(11);
 			cell.setCellValue(signalDTO.getAlgorithmPRRLB());
-
-			cell = row.createCell(6);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(12);
 			cell.setCellValue(signalDTO.getAlgorithmPRRUB());
-
-			cell = row.createCell(7);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(13);
 			cell.setCellValue(signalDTO.getAlgorithmRORScore());
-
-			cell = row.createCell(8);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(14);
 			cell.setCellValue(signalDTO.getAlgorithmRORLB());
-
-			cell = row.createCell(9);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(15);
 			cell.setCellValue(signalDTO.getAlgorithmRORUB());
-
-			cell = row.createCell(10);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(16);
 			cell.setCellValue(signalDTO.getAlgorithmRRRScore());
-
-			cell = row.createCell(11);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(17);
 			cell.setCellValue(signalDTO.getAlgorithmRRRLB());
-
-			cell = row.createCell(12);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(18);
 			cell.setCellValue(signalDTO.getAlgorithmRRRUB());
-
-			cell = row.createCell(13);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(19);
 			cell.setCellValue(signalDTO.getAlgorithmEBScore());
-
-			cell = row.createCell(14);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(20);
 			cell.setCellValue(signalDTO.getAlgorithmBCPNNScore());
-
-			cell = row.createCell(15);
-			cell.setCellStyle(style);
+			
+			cell=row.createCell(21);
 			cell.setCellValue(signalDTO.getPp());
-			cell = row.createCell(16);
-			cell.setCellStyle(style);
+			cell=row.createCell(22);
 			cell.setCellValue(signalDTO.getSignal());
 
 		}
