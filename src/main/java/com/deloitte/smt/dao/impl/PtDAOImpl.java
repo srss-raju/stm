aca56166ca851a1f24242ac9af62ccb325458b0c
RajeshKumar B <rbongurala@innominds.com>
2017-08-26 15:34:06 +0530

    pfizer schema is added

--- src/main/java/com/deloitte/smt/dao/impl/PtDAOImpl.java
+++ src/main/java/com/deloitte/smt/dao/impl/PtDAOImpl.java
@@ -13,14 +13,14 @@ import com.deloitte.smt.util.PtMapper;
 public class PtDAOImpl implements PtDAO{
 
 private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
-    
+
     public void setNamedParameterJdbcTemplate(NamedParameterJdbcTemplate namedParameterJdbcTemplate){
         this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
     }
 
 	@Override
 	public List<PtDTO> findPtsBySmqId(List<Integer> ids) {
-		String query = "SELECT smq_cui_key, meddra_pt_cui_key, pt_name FROM smq_meddra_mapping where smq_cui_key IN (:ids) order by smq_cui_key";
+		String query = "SELECT smq_cui_key, meddra_pt_cui_key, pt_name FROM pfizer.smq_meddra_mapping where smq_cui_key IN (:ids) order by smq_cui_key";
 		Map<String, Object> params = new HashMap<>();
 		params.put("ids", ids);
 		return namedParameterJdbcTemplate.query(query, params, new PtMapper());
