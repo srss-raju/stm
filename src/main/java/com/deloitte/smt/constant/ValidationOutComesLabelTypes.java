487572f52f19e078767dac8c7edc9a12d343a767
RajeshKumar B <rbongurala@innominds.com>
2017-09-06 15:34:50 +0530

    Validated to modified

--- src/main/java/com/deloitte/smt/constant/ValidationOutComesLabelTypes.java
+++ src/main/java/com/deloitte/smt/constant/ValidationOutComesLabelTypes.java
@@ -2,7 +2,7 @@ package com.deloitte.smt.constant;
 
 public  class ValidationOutComesLabelTypes {
 	public static final String VALIDATED_SIGNAL_WITH_RISK="Validated Signal With Risk";
-	public static final String VALIDATED_SIGNAL_WITHOUT_RISK="Valiated Signal Without Risk";
+	public static final String VALIDATED_SIGNAL_WITHOUT_RISK="Validated Signal Without Risk";
 	public static final String NON_SIGNAL="Non-Signal";
 	public static final String CONTINUE_TO_MONTOR="Continue to Montior";
 	
