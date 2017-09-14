146d53e68ce86d15777d7e3a74c49a5fa25ac835
RajeshKumar B <rbongurala@innominds.com>
2017-09-15 00:52:51 +0530

    added testmethods

--- src/test/java/com/deloitte/smt/servicetest/AssignmentConfigurationServiceTest.java
+++ src/test/java/com/deloitte/smt/servicetest/AssignmentConfigurationServiceTest.java
@@ -2,6 +2,9 @@ package com.deloitte.smt.servicetest;
 
 import static org.mockito.BDDMockito.given;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.log4j.Logger;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngineConfiguration;
@@ -19,8 +22,14 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.deloitte.smt.SignalManagementApplication;
+import com.deloitte.smt.entity.AssessmentAssignmentAssignees;
 import com.deloitte.smt.entity.AssignmentConfiguration;
+import com.deloitte.smt.entity.RiskPlanAssignmentAssignees;
+import com.deloitte.smt.entity.SignalValidationAssignmentAssignees;
+import com.deloitte.smt.repository.AssessmentAssignmentAssigneesRepository;
 import com.deloitte.smt.repository.AssignmentConfigurationRepository;
+import com.deloitte.smt.repository.RiskPlanAssignmentAssigneesRepository;
+import com.deloitte.smt.repository.SignalValidationAssignmentAssigneesRepository;
 import com.deloitte.smt.service.AssignmentConfigurationService;
 
 @RunWith(SpringRunner.class)
@@ -35,6 +44,15 @@ public class AssignmentConfigurationServiceTest {
 	
 	@MockBean
 	AssignmentConfigurationRepository assignmentConfigurationRepository;
+	
+	@MockBean
+    SignalValidationAssignmentAssigneesRepository signalValidationAssignmentAssigneesRepository;
+    
+	@MockBean
+    AssessmentAssignmentAssigneesRepository assessmentAssignmentAssigneesRepository;
+    
+	@MockBean
+    RiskPlanAssignmentAssigneesRepository riskPlanAssignmentAssigneesRepository;
 	
 		
 	private static final ProcessEngineConfiguration processEngineConfiguration = new StandaloneInMemProcessEngineConfiguration() {
@@ -63,6 +81,42 @@ public class AssignmentConfigurationServiceTest {
 			config.setAssessmentAssignmentOwner("Test User");
 			config.setIngredient("test");
 			config.setSignalSource("test");
+			
+			List<SignalValidationAssignmentAssignees> svList = new ArrayList<>();
+			SignalValidationAssignmentAssignees svAssignee = new SignalValidationAssignmentAssignees();
+			svAssignee.setUserGroupKey(1l);
+			svList.add(svAssignee);
+			
+			List<AssessmentAssignmentAssignees> aList = new ArrayList<>();
+			AssessmentAssignmentAssignees aAssignee = new AssessmentAssignmentAssignees();
+			svAssignee.setUserGroupKey(1l);
+			aList.add(aAssignee);
+			
+			List<RiskPlanAssignmentAssignees> rpList = new ArrayList<>();
+			RiskPlanAssignmentAssignees rAssignee = new RiskPlanAssignmentAssignees();
+			svAssignee.setUserGroupKey(1l);
+			rpList.add(rAssignee);
+			
+			config.setSignalValidationAssignmentAssignees(svList);
+			config.setAssessmentAssignmentAssignees(aList);
+			config.setRiskPlanAssignmentAssignees(rpList);
+			
+			//given(this.assignmentConfigurationRepository.findByIngredientAndSignalSource("test","test")).willReturn(config);
+			given(this.assignmentConfigurationRepository.save(config)).willReturn(config);
+			assignmentConfigurationService.insert(config);
+		}catch(Exception ex){
+			
+		}
+	}
+	
+	@Test
+	public void testInsertWithException() {
+		try{
+			AssignmentConfiguration config = new AssignmentConfiguration();
+			config.setAssessmentAssignmentOwner("Test User");
+			config.setIngredient("test");
+			config.setSignalSource("test");
+			
 			given(this.assignmentConfigurationRepository.findByIngredientAndSignalSource("test","test")).willReturn(config);
 			assignmentConfigurationService.insert(config);
 		}catch(Exception ex){
@@ -169,6 +223,78 @@ public class AssignmentConfigurationServiceTest {
 	@Test
 	public void testFindAll() throws Exception{
 		assignmentConfigurationService.findAll();
+	}
+	
+	@Test
+	public void testDeleteSignalValidationAssignmentAssignee() throws Exception{
+		try{
+			SignalValidationAssignmentAssignees assignee = new SignalValidationAssignmentAssignees();
+			assignee.setId(1l);
+			given(this.signalValidationAssignmentAssigneesRepository.findOne(1l)).willReturn(assignee);
+			assignmentConfigurationService.deleteSignalValidationAssignmentAssignee(1l);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testDeleteSignalValidationAssignmentAssigneeWithNull() throws Exception{
+		try{
+			SignalValidationAssignmentAssignees assignee = new SignalValidationAssignmentAssignees();
+			assignee.setId(1l);
+			given(this.signalValidationAssignmentAssigneesRepository.findOne(11l)).willReturn(assignee);
+			assignmentConfigurationService.deleteSignalValidationAssignmentAssignee(1l);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testDeleteAssessmentAssignmentAssignee() throws Exception{
+		try{
+			AssessmentAssignmentAssignees assignee = new AssessmentAssignmentAssignees();
+			assignee.setId(1l);
+			given(this.assessmentAssignmentAssigneesRepository.findOne(1l)).willReturn(assignee);
+			assignmentConfigurationService.deleteSignalValidationAssignmentAssignee(1l);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testDeleteAssessmentAssignmentAssigneeWithNull() throws Exception{
+		try{
+			AssessmentAssignmentAssignees assignee = new AssessmentAssignmentAssignees();
+			assignee.setId(1l);
+			given(this.assessmentAssignmentAssigneesRepository.findOne(11l)).willReturn(assignee);
+			assignmentConfigurationService.deleteSignalValidationAssignmentAssignee(1l);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testDeleteRiskPlanAssignmentAssignee() throws Exception{
+		try{
+			RiskPlanAssignmentAssignees assignee = new RiskPlanAssignmentAssignees();
+			assignee.setId(1l);
+			given(this.riskPlanAssignmentAssigneesRepository.findOne(1l)).willReturn(assignee);
+			assignmentConfigurationService.deleteRiskPlanAssignmentAssignee(1l);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
+	}
+	
+	@Test
+	public void testDeleteRiskPlanAssignmentAssigneeWithNull() throws Exception{
+		try{
+			RiskPlanAssignmentAssignees assignee = new RiskPlanAssignmentAssignees();
+			assignee.setId(1l);
+			given(this.riskPlanAssignmentAssigneesRepository.findOne(11l)).willReturn(assignee);
+			assignmentConfigurationService.deleteRiskPlanAssignmentAssignee(1l);
+		}catch(Exception ex){
+			LOG.info(ex);
+		}
 	}
 	
 }
