71f70a65378bdffd9deb6a2ef8d7ba7d66a203e4
shashikala <shbondada@innominds.com>
2017-11-16 16:41:35 +0530

    Removed he unused imports

--- src/main/java/com/deloitte/smt/controller/MedraBrowserController.java
+++ src/main/java/com/deloitte/smt/controller/MedraBrowserController.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
--- src/main/java/com/deloitte/smt/entity/Llt.java
+++ src/main/java/com/deloitte/smt/entity/Llt.java
@@ -6,11 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 @Table(name = "sm_llt")
