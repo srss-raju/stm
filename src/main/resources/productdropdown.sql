64464c8968e06bf862a3a661ae15ad3647d9974b
RajeshKumar B <rbongurala@innominds.com>
2017-12-06 15:49:05 +0530

    values modified

--- src/main/resources/productdropdown.sql
+++ src/main/resources/productdropdown.sql
@@ -1,24 +1,24 @@
 Insert into sm_product_level
    (id, key,value, versions)
  Values
-   ('1', 'ATC_LVL_1','ATC_LVL_1', '20.0J');
+   ('1', 'ATC_LVL_1','ANATOMICAL MAINGROUP', '20.0J');
    
 Insert into sm_product_level
    (id, key,value,  versions)
  Values
-   ('2','ATC_LVL_2', 'ATC_LVL_2', '20.0J');
+   ('2','ATC_LVL_2', 'THERAPEUTIC MAINGROUP', '20.0J');
    
 Insert into sm_product_level
    (id, key,value,  versions)
  Values
-   ('3', 'ATC_LVL_3','ATC_LVL_3', '20.0J');
+   ('3', 'ATC_LVL_3','THERAPEUTIC PHARMACOLOGICAL SUBGROUP', '20.0J');
    
 Insert into sm_product_level
    (id, key,value,  versions)
  Values
-   ('4','ATC_LVL_4', 'ATC_LVL_4', '20.0J');
+   ('4','ATC_LVL_4', 'CHEMICAL THERAPEUTIC PHARMACOLOGICAL SUBGROUP', '20.0J');
    
 Insert into sm_product_level
    (id, key,value,  versions)
  Values
-   ('5','ATC_LVL_5', 'ATC_LVL_5', '20.0J');
+   ('5','ATC_LVL_5', 'CHEMICAL SUBSTANCE', '20.0J');
