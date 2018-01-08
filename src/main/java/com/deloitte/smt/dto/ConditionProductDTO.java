51afcdd12f1ed9a6a7723f617b7332490684755a
RajeshKumar B <rbongurala@innominds.com>
2018-01-08 14:57:43 +0530

    DTO is modified

--- src/main/java/com/deloitte/smt/dto/ConditionProductDTO.java
+++ src/main/java/com/deloitte/smt/dto/ConditionProductDTO.java
@@ -3,30 +3,19 @@ package com.deloitte.smt.dto;
 import java.io.Serializable;
 import java.util.List;
 
-import com.deloitte.smt.entity.ProductAssignmentConfiguration;
-import com.deloitte.smt.entity.SocAssignmentConfiguration;
+import lombok.Data;
 
+import com.deloitte.smt.entity.AssignmentCondition;
+import com.deloitte.smt.entity.AssignmentProduct;
+
+@Data
 public class ConditionProductDTO implements Serializable{
 
 	private static final long serialVersionUID = -7910268136658523849L;
 	
-	private List<SocAssignmentConfiguration> conditions;
+	private List<AssignmentCondition> conditions;
 	
-	private List<ProductAssignmentConfiguration> products;
+	private List<AssignmentProduct> products;
 
-	public List<SocAssignmentConfiguration> getConditions() {
-		return conditions;
-	}
-
-	public void setConditions(List<SocAssignmentConfiguration> conditions) {
-		this.conditions = conditions;
-	}
-
-	public List<ProductAssignmentConfiguration> getProducts() {
-		return products;
-	}
-
-	public void setProducts(List<ProductAssignmentConfiguration> products) {
-		this.products = products;
-	}
+	
 }
