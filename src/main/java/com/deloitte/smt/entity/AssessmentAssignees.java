e0c723b977ea1718f88d578296881cea6a87428e
sbandepalli <sbandepalli@IM-LP-428.innomindshyd.com>
2018-01-11 11:56:25 +0530

    Refactor

--- src/main/java/com/deloitte/smt/entity/AssessmentAssignees.java
+++ src/main/java/com/deloitte/smt/entity/AssessmentAssignees.java
@@ -10,11 +10,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
-import javax.persistence.Transient;
-
-import lombok.Data;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import lombok.Data;
 
 /**
  * Created by Rajesh on 31-07-2017.
@@ -22,37 +21,24 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Data
 @Entity
 @Table(name = "sm_assessment_assignees")
-public class AssessmentAssignees implements Serializable{
-	 
-		/**
-	 * 
-	 */
+public class AssessmentAssignees implements Serializable {
+
+	/**
+	* 
+	*/
 	private static final long serialVersionUID = -6586959436931561167L;
 
-		@Id
-	    @GeneratedValue(strategy = GenerationType.AUTO)
-	    private Long id;
-		private Date createdDate;
-	    private String createdBy;
-	    private Date lastModifiedDate;
-	    private String assignTo;
-	    private String userGroupKey;
-	    private String userKey;
-	    
-	    private Long assessmentId;
-	   
-	    
-	    @JsonIgnore
-	    private AssessmentPlan assessmentPlan;
-	    
-	    @ManyToOne
-		@JoinColumn(name = "assessmentId")
-	    public AssessmentPlan getAssessmentPlan() {
-			return assessmentPlan;
-		}
-		public void setAssessmentPlan(AssessmentPlan assessmentPlan) {
-			this.assessmentPlan = assessmentPlan;
-		}
-	    
-		
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private Date createdDate;
+	private String createdBy;
+	private Date lastModifiedDate;
+	private String userGroupKey;
+	private String userKey;
+	
+	@ManyToOne
+	@JoinColumn(name = "assessmentPlanId")
+	@JsonIgnore
+	private AssessmentPlan assessmentPlan;
 }
