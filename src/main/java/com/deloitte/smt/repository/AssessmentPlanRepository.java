7e4d710ab08efefed93a4d682fcd153324e5955e
sbandepalli <sbandepalli@IM-LP-428.innomindshyd.com>
2018-01-11 15:46:12 +0530

    Refactor

--- src/main/java/com/deloitte/smt/repository/AssessmentPlanRepository.java
+++ src/main/java/com/deloitte/smt/repository/AssessmentPlanRepository.java
@@ -15,16 +15,7 @@ import com.deloitte.smt.entity.Topic;
 
 public interface AssessmentPlanRepository extends JpaRepository<AssessmentPlan, Long> {
 
-	@Query(value = "SELECT t.id FROM AssessmentPlan o inner join o.topics t")
-	Set<Long> findAllSignalIds();
-
 	List<AssessmentPlan> findAllByCreatedDateOrderByCreatedDateDesc(Date createdDate);
-	
-	@Query(value = "SELECT o FROM AssessmentPlan o WHERE o.assignTo IN (:assignes)  ORDER BY o.createdDate DESC")
-	List<AssessmentPlan> findAllByAssignToInOrderByCreatedDateDesc(@Param("assignes") List<String> assignes);
-
-	@Query("SELECT o FROM AssessmentPlan o WHERE o.assessmentPlanStatus IN (:assessmentPlanStatus) AND o.createdDate = :createdDate ORDER BY o.createdDate DESC")
-	List<AssessmentPlan> findAllByAssessmentPlanStatusAndCreatedDate(@Param("assessmentPlanStatus") List<String> assessmentPlanStatus, @Param("createdDate") Date createdDate);
 	
 	@Query("SELECT DISTINCT(o.assessmentRiskStatus) FROM AssessmentPlan o WHERE o.assessmentRiskStatus IS NOT NULL")
 	List<String> getAssessmentRiskStatus();
@@ -36,13 +27,7 @@ public interface AssessmentPlanRepository extends JpaRepository<AssessmentPlan, 
 	
 	Long countByAssessmentNameIgnoreCase(String assessmentName);
 	
-	@Query("SELECT DISTINCT(o.owner) FROM AssessmentPlan o WHERE o.owner IS NOT NULL")
-	List<String> findOwnersOnAssessmentPlan();
-	
 	@Query("SELECT o.assessmentName from AssessmentPlan o where o.assessmentName not in (select a.assessmentName from AssessmentPlan a where a.assessmentName= :assessmentName AND a.id=:id)")
 	List<String> findByAssessmentName(@Param(value = "assessmentName") String assessmentName,@Param(value = "id") Long id);
-	
-	@Query(value = "SELECT t FROM AssessmentPlan o inner join o.topics t  WHERE o.id= :id order by t.id desc")
-	Set<Topic> findAllSignals(@Param(value="id") Long id);
 	
 }
