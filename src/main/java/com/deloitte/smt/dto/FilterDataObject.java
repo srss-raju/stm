5766017eab3babd9e77f16702948ae08f9777568
jshaik <jshaik@innominds.com>
2018-01-23 16:23:19 +0530

    @JsonFormat(pattern = "dd-MM-yyyy") added

--- src/main/java/com/deloitte/smt/dto/FilterDataObject.java
+++ src/main/java/com/deloitte/smt/dto/FilterDataObject.java
@@ -1,5 +1,6 @@
 package com.deloitte.smt.dto;
 
+import java.io.Serializable;
 import java.util.Date;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
@@ -7,22 +8,28 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 @Data
-public class FilterDataObject {
+public class FilterDataObject implements Serializable{
+	private static final long serialVersionUID = 5249048523110491930L;
 	private Object id;
 	private String name;
 	private String status;
 	private String description;
 	private String signalConfirmation;
 	private String sourceName;
+	@JsonFormat(pattern = "dd-MM-yyyy")
 	private Date createdDate;
 	private String priority;
+	@JsonFormat(pattern = "dd-MM-yyyy")
 	private Date dueDate;
 	private String runFrequency;
 	private Object denominatorForPoisson;
+	@JsonFormat(pattern = "dd-MM-yyyy")
 	private Date lastRunDate;
 	private Object signalDetected;
+	@JsonFormat(pattern = "dd-MM-yyyy")
 	private Date nextRunDate;
 	private String assessmentName;
+	@JsonFormat(pattern = "dd-MM-yyyy")
 	private Date assessmentDueDate;
 	private String assessmentPlanStatus;
 	
