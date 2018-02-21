7efa7daaf384539ed4d3800c3ef260db6ef35ffa
RajeshKumar B <rbongurala@innominds.com>
2018-02-21 15:22:41 +0530

    ConditionFilterServiceTest is added

---
+++ src/test/java/com/deloitte/smt/servicetest/ConditionFilterServiceTest.java
@@ -0,0 +1,120 @@
+package com.deloitte.smt.servicetest;
+
+import static org.mockito.BDDMockito.given;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.deloitte.smt.SignalManagementApplication;
+import com.deloitte.smt.dto.FilterDTO;
+import com.deloitte.smt.entity.ConditionLevels;
+import com.deloitte.smt.repository.AssessmentPlanRepository;
+import com.deloitte.smt.repository.AssignmentConditionValuesRepository;
+import com.deloitte.smt.repository.ConditionLevelRepository;
+import com.deloitte.smt.repository.RiskPlanRepository;
+import com.deloitte.smt.repository.SignalDetectionRepository;
+import com.deloitte.smt.repository.TopicRepository;
+import com.deloitte.smt.service.impl.ConditionFilterServiceImpl;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=SignalManagementApplication.class)
+@TestPropertySource(locations = {"classpath:test.properties"})
+public class ConditionFilterServiceTest {
+
+	@Autowired
+	ConditionFilterServiceImpl conditionFilterServiceImpl;
+	
+	@MockBean
+	private ConditionLevelRepository conditionLevelRepository;
+
+	@MockBean
+	private TopicRepository topicRepository;
+
+	@MockBean
+	private AssignmentConditionValuesRepository assignmentConditionValuesRepository;
+
+	@MockBean
+	private SignalDetectionRepository signalDetectionRepository;
+
+	@MockBean
+	private RiskPlanRepository riskPlanRepository;
+
+	@MockBean
+	private AssessmentPlanRepository assessmentPlanRepository;
+	
+	@Test
+	public void testGetSignalFilters() {
+		try{
+			List<FilterDTO> filterList = new ArrayList<>();
+			List<String> conFillVals = new ArrayList<>();
+			List<ConditionLevels> conditionLevels = new ArrayList<>();
+			ConditionLevels conditionLevel = new ConditionLevels();
+			conditionLevels.add(conditionLevel);
+			
+			conFillVals.add("1@#2");
+			given(this.topicRepository.getConditionFilterValues()).willReturn(conFillVals);
+			conditionFilterServiceImpl.conditionLevelFilter(filterList, "signal");
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testGetDetectionFilters() {
+		try{
+			List<FilterDTO> filterList = new ArrayList<>();
+			List<String> conFillVals = new ArrayList<>();
+			List<ConditionLevels> conditionLevels = new ArrayList<>();
+			ConditionLevels conditionLevel = new ConditionLevels();
+			conditionLevels.add(conditionLevel);
+			
+			conFillVals.add("1@#2");
+			given(this.topicRepository.getConditionFilterValues()).willReturn(conFillVals);
+			conditionFilterServiceImpl.conditionLevelFilter(filterList, "detection");
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testGetRiskFilters() {
+		try{
+			List<FilterDTO> filterList = new ArrayList<>();
+			List<String> conFillVals = new ArrayList<>();
+			List<ConditionLevels> conditionLevels = new ArrayList<>();
+			ConditionLevels conditionLevel = new ConditionLevels();
+			conditionLevels.add(conditionLevel);
+			
+			conFillVals.add("1@#2");
+			given(this.topicRepository.getConditionFilterValues()).willReturn(conFillVals);
+			conditionFilterServiceImpl.conditionLevelFilter(filterList, "risk");
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	@Test
+	public void testGetAssessmentFilters() {
+		try{
+			List<FilterDTO> filterList = new ArrayList<>();
+			List<String> conFillVals = new ArrayList<>();
+			List<ConditionLevels> conditionLevels = new ArrayList<>();
+			ConditionLevels conditionLevel = new ConditionLevels();
+			conditionLevels.add(conditionLevel);
+			
+			conFillVals.add("1@#2");
+			given(this.topicRepository.getConditionFilterValues()).willReturn(conFillVals);
+			conditionFilterServiceImpl.conditionLevelFilter(filterList, "assessment");
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+}
