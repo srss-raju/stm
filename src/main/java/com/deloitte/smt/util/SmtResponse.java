0e7e2e2bdee075d216ff8f34b044fde48bad0e62
RajeshKumar B <rbongurala@innominds.com>
2018-03-01 23:40:08 +0530

    lambok is added

--- src/main/java/com/deloitte/smt/util/SmtResponse.java
+++ src/main/java/com/deloitte/smt/util/SmtResponse.java
@@ -2,11 +2,14 @@ package com.deloitte.smt.util;
 
 import java.util.List;
 
+import lombok.Data;
+
 /**
  * 
  * @author rbongurala
  *
  */
+@Data
 public class SmtResponse {
 	
 	private List<?> result;
@@ -14,37 +17,5 @@ public class SmtResponse {
 	private int totalRecords;
 	private int fetchSize;
 	private int fromRecord;
-	
-	
-	public List<?> getResult() {
-		return result;
-	}
-	public void setResult(List<?> result) {
-		this.result = result;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
-	public int getTotalRecords() {
-		return totalRecords;
-	}
-	public void setTotalRecords(int totalRecords) {
-		this.totalRecords = totalRecords;
-	}
-	public int getFetchSize() {
-		return fetchSize;
-	}
-	public void setFetchSize(int fetchSize) {
-		this.fetchSize = fetchSize;
-	}
-	public int getFromRecord() {
-		return fromRecord;
-	}
-	public void setFromRecord(int fromRecord) {
-		this.fromRecord = fromRecord;
-	}
 	
 }
